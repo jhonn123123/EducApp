3cf3caa35d889aaed85fd571b90d34ad9005dd43
jhonn123123 <juan01mbg@gmail.com>
2020-09-30 18:46:49 -0500

    bottons and press effect

--- app/src/main/java/com/example/educapp/MainActivity.kt
+++ app/src/main/java/com/example/educapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.educapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       btn_jugar.setOnClickListener {
+           val intent = Intent( this,level::class.java)
+           startActivity(intent)
+       }
+
+
     }
 }
---
+++ app/src/main/java/com/example/educapp/level.kt
@@ -0,0 +1,11 @@
+package com.example.educapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class level : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level)
+    }
+}
